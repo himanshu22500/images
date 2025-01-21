23f897f457386c1c01fd2a1dfe359bde77699401
himanshu.mishra <himanshu@edralabs.com>
2025-01-21 09:54:17 +0530

    lint

--- mower_downloads.py
+++ mower_downloads.py
@@ -9,25 +9,15 @@ from crawl4ai.async_configs import BrowserConfig, CrawlerRunConfig
 
 load_dotenv()
 mower_data = [
-{"id":"1290","slug":"ryobi-p1180","name":"P1180","page_url":"https://www.ryobitools.com/products/details/46396034571","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1291","slug":"ryobi-p1190","name":"P1190","page_url":"https://www.ryobitools.com/products/details/46396034588","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1301","slug":"ryobi-ry401220","name":"RY401220","page_url":"https://www.ryobitools.com/products/details/46396040718","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1292","slug":"ryobi-ry401140","name":"RY401140","page_url":"https://www.ryobitools.com/products/details/46396036995","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1296","slug":"ryobi-ry401101","name":"RY401101","page_url":"https://www.ryobitools.com/products/details/46396045607","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1298","slug":"ryobi-ry401210","name":"RY401210","page_url":"https://www.ryobitools.com/products/details/46396040244","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1299","slug":"ryobi-ry401170","name":"RY401170","page_url":"https://www.ryobitools.com/products/details/46396037169","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1300","slug":"ryobi-ry401180","name":"RY401180","page_url":"https://www.ryobitools.com/products/details/46396037176","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1302","slug":"ryobi-ry401260","name":"RY401260","page_url":"https://www.ryobitools.com/products/details/46396045386","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1304","slug":"ryobi-ryrm8010","name":"RYRM8010","page_url":"https://www.ryobitools.com/products/details/46396040886","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1307","slug":"ryobi-ryrm8060","name":"RYRM8060","page_url":"https://www.ryobitools.com/products/details/46396041845","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1308","slug":"ryobi-ryrm8070","name":"RYRM8070","page_url":"https://www.ryobitools.com/products/details/46396041852","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1293","slug":"ryobi-ry401150","name":"RY401150","page_url":"https://www.ryobitools.com/products/details/46396036711","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1294","slug":"ryobi-ry48ztr100","name":"RY48ZTR100","page_url":"https://www.ryobitools.com/products/details/46396025395","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1295","slug":"ryobi-ry48ztr75","name":"RY48ZTR75","page_url":"https://www.ryobitools.com/products/details/46396023971","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1297","slug":"ryobi-ry401100vnm","name":"RY401100VNM","page_url":"https://www.ryobitools.com/products/details/46396027726","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1303","slug":"ryobi-rypm8010","name":"RYPM8010","page_url":"https://www.ryobitools.com/products/details/46396046468","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1305","slug":"ryobi-ryrm8021","name":"RYRM8021","page_url":"https://www.ryobitools.com/products/details/46396040923","brand_name":"Ryobi","category_page_url":"","series_page_url":""},
-{"id":"1306","slug":"ryobi-ryrm8034","name":"RYRM8034","page_url":"https://www.ryobitools.com/products/details/46396041029","brand_name":"Ryobi","category_page_url":"","series_page_url":""}
+    {
+        "id": "1290",
+        "slug": "ryobi-p1180",
+        "name": "P1180",
+        "page_url": "https://www.ryobitools.com/products/details/46396034571",
+        "brand_name": "Ryobi",
+        "category_page_url": "",
+        "series_page_url": "",
+    },
 ]
 
 
@@ -45,16 +35,19 @@ async def click_and_extract_images(
 ):
     browser_config = BrowserConfig(browser_type="chromium", headless=True)
     llm_strategy = LLMExtractionStrategy(
-            provider="openai/gpt-4o",
-            api_token=os.getenv("OPENAI_API_KEY"),
-            extraction_type="schema",
-            schema=ImageLink.model_json_schema(),
-            instruction="""
+        provider="openai/gpt-4o",
+        api_token=os.getenv("OPENAI_API_KEY"),
+        extraction_type="schema",
+        schema=ImageLink.model_json_schema(),
+        instruction="""
         Extract all mower model image links from the page.
         """,
-        )
+    )
     crawler_run_config = CrawlerRunConfig(
-        wait_for_images=True, verbose=True, scan_full_page=True, scroll_delay=3,
+        wait_for_images=True,
+        verbose=True,
+        scan_full_page=True,
+        scroll_delay=3,
         extraction_strategy=llm_strategy,
         cache_mode=CacheMode.DISABLED,
     )
